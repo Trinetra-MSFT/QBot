1aed978d84bc94747cc8341bae23d01dc915eae6
Trinetra-MSFT <v-trkum@microsoft.com>
2021-01-27 18:45:19 +0530

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdeploytnt.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://QBotBaseLocal.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
