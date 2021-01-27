e16f73a5a98604e7d7bf8db1694c0bcd11ba5436
Trinetra-MSFT <v-trkum@microsoft.com>
2021-01-27 18:43:57 +0530

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://QBotBaseLocal.azurewebsites.net/api/Request/",
+    selfUrl: "https://QBotBaseLocal-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "d634c2da-9cba-459a-aad1-81df852f770f",
+        clientId: "c717db7b-8445-4198-acec-aaa6fa5ff882",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
